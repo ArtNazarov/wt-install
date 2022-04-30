67f5464ac12bf4eaf3fcdd82128aeebad0270215
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2022-04-30 17:46:16 +0500

    Update inst-from-mirror.sh

--- inst-from-mirror.sh
+++ inst-from-mirror.sh
@@ -8,3 +8,8 @@ wget https://mirror.artnazarov.ru/vcfull.zip
 unzip vcfull.zip
 wine VCR-2005-2019-14.10.2020.exe 
 rm vcfull.zip
+# NET 4.8   
+wget https://mirror.artnazarov.ru/net48.zip
+unzip net48.zip
+wine NET.Framework-4.8.exe
+rm net48.zip
