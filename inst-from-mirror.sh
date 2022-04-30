35a8b9ae74edfce08b5362a1975f18a2bc754ca4
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2022-04-30 17:49:43 +0500

    Update inst-from-mirror.sh

--- inst-from-mirror.sh
+++ inst-from-mirror.sh
@@ -1,15 +1,15 @@
 # Runtime Pack Lite
-wget https://mirror.artnazarov.ru/runtimepack.zip
+wget http://mirror.artnazarov.ru/runtimepack.zip
 unzip runtimepack.zip
 wine RuntimePack_Lite-20.3.3.exe
 rm runtimepack.zip
 # VC full 2005-2019              
-wget https://mirror.artnazarov.ru/vcfull.zip
+wget http://mirror.artnazarov.ru/vcfull.zip
 unzip vcfull.zip
 wine VCR-2005-2019-14.10.2020.exe 
 rm vcfull.zip
 # NET 4.8   
-wget https://mirror.artnazarov.ru/net48.zip
+wget http://mirror.artnazarov.ru/net48.zip
 unzip net48.zip
 wine NET.Framework-4.8.exe
 rm net48.zip
