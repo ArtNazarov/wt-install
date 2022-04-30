92aea60ae05c3c143326f63dbd2d0686553557a0
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2022-04-30 17:32:52 +0500

    Create inst-from-mirror.sh

---
+++ inst-from-mirror.sh
@@ -0,0 +1,6 @@
+# software as is and not checked by antivirus
+# Runtime Pack Lite
+wget https://mirror.artnazarov.ru/runtimepack.zip
+unzip runtimepack.zip
+wine RuntimePack_Lite-20.3.3.exe
+rm runtimepack.zip
