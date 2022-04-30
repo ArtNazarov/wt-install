e3af39e766482f388afb2b16bf89ed2a5a156cf8
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2022-04-30 18:44:38 +0500

    Update inst-from-mirror.sh

--- inst-from-mirror.sh
+++ inst-from-mirror.sh
@@ -1,38 +1,40 @@
 # Runtime Pack Lite
 cd ~
-DFILE=runtimepack.exe
+export DFILE=runtimepack.exe
 if [ ! -f "~/$DFILE" ]; then
-    echo "$DFILE exists. Skip downloading"
+    echo "$DFILE not exists. Skip downloading"
+    wget http://mirror.artnazarov.ru/runtimepack.zip
+    unzip runtimepack.zip
+    rm runtimepack.zip -f
 else
 echo "Installing"
-wget http://mirror.artnazarov.ru/runtimepack.zip
-unzip runtimepack.zip
-fi
-
-rm runtimepack.zip -f
 wine RuntimePack_Lite-20.3.3.exe
 rm RuntimePack_Lite-20.3.3.exe -f
+fi
+
+
+
 export DFILE=VCRHyb64.exe
 if [ ! -f "~/$DFILE" ]; then
     echo "$DFILE exists. Skip downloading"
+    wget http://mirror.artnazarov.ru/VCRHyb64.zip
+    unzip VCRHyb64.exe
+    rm VCRHyb64.zip -f
 else
 echo "Installing"
-# VC full 2005-2019              
-wget http://mirror.artnazarov.ru/VCRHyb64.zip
-unzip VCRHyb64.exe
-rm VCRHyb64.zip -f
-fi
 wine VCRHyb64.exe
-rm VCRHyb64.exe -f
+rm VCRHyb64.exe -f          
+fi
+
 # NET 4.8  
 export DFILE=VCRHyb64.exe
 if [ ! -f "~/$DFILE" ]; then
     echo "$DFILE exists. Skip downloading"
+    wget http://mirror.artnazarov.ru/net48.zip
+    unzip net48.zip 
+    rm net48.zip -f
 else
 echo "Installing..."
-wget http://mirror.artnazarov.ru/net48.zip
-unzip net48.zip 
-rm net48.zip -f
-fi
 wine NET.Framework-4.8.exe
 rm NET.Framework-4.8.exe -f
+fi
