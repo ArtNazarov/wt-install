e62fd057d04a31ff4b77a5c92e8af24f041b121c
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2022-04-30 17:38:34 +0500

    Update inst-from-mirror.sh

--- inst-from-mirror.sh
+++ inst-from-mirror.sh
@@ -1,6 +1,10 @@
-# software as is and not checked by antivirus
 # Runtime Pack Lite
 wget https://mirror.artnazarov.ru/runtimepack.zip
 unzip runtimepack.zip
 wine RuntimePack_Lite-20.3.3.exe
 rm runtimepack.zip
+# VC full 2005-2019              
+wget https://mirror.artnazarov.ru/vcfull.zip
+unzip vcfull.zip
+wine VCR-2005-2019-14.10.2020.exe 
+rm vcfull.zip
