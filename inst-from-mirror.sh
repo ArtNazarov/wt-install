c81ce757c7ada1e06061c168ba4ce1e52a7f8e04
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2022-04-30 21:07:05 +0500

    Update inst-from-mirror.sh

--- inst-from-mirror.sh
+++ inst-from-mirror.sh
@@ -1,10 +1,10 @@
 # Runtime Pack Lite
 cd ~
 
-declare -a libs=("RuntimePack_Lite-20.3.3.exe" "VCRHyb64.exe" "NET.Framework-4.8.exe")
-declare -a zips=("runtimepack.zip" "VCRHyb64.zip" "net48.zip")
+declare -a libs=("RuntimePack_Lite-20.3.3.exe" "VCRHyb64.exe" "NET.Framework-4.8.exe" "K-Lite_Mega_Codec_Pack-15.8.5.exe")
+declare -a zips=("runtimepack.zip" "VCRHyb64.zip" "net48.zip" "codecs.zip")
 
-for index in {0..2}; do
+for index in {0..3}; do
 echo "Шаг $index"
 lib="${libs[$index]}"
 zfile="${zips[$index]}"
@@ -15,10 +15,10 @@ echo "имя архива - $zfile"
 if [ ! -f "$lib" ]
 then
    echo "$lib not exists. Wait while downloading"
-    echo "http://mirror.artnazarov.ru/$zfile"
-    wget "http://mirror.artnazarov.ru/$zfile"
-    unzip $zfile
-    rm $zfile -f
+   echo "http://mirror.artnazarov.ru/$zfile"
+   wget "http://mirror.artnazarov.ru/$zfile"
+   unzip $zfile
+   rm $zfile -f
 else
    echo "Installing"
 fi
@@ -27,5 +27,3 @@ wine $lib
 rm $lib -f
   
 done
-
-
